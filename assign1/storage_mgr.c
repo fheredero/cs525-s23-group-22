5efb1f8946c171b6c6241625f81a2432f667e25e
jbenegassi <jmendezbenegassimarq@hawk.iit.edu>
2023-02-05 21:31:18 -0600

    Update storage_mgr.c

--- assign1/storage_mgr.c
+++ assign1/storage_mgr.c
@@ -29,7 +29,7 @@ extern RC createPageFile (char *fileName){
     fHeader.totalNumPages = 1;
     fHeader.curPagePos = 0;
     fwrite(&fHeader,sizeof(fHeader),1,file);
-    char *charArray = calloc(PAGE_SIZE);
+    char *charArray = calloc(PAGE_SIZE, 1);
     int write = fwrite(charArray, 1, PAGE_SIZE, file);
     fclose(file);
     if (write != PAGE_SIZE){ 			
@@ -163,8 +163,7 @@ extern RC appendEmptyBlock (SM_FileHandle *fHandle){
     SM_FileHeader fHeader;
     fread(&fHeader, sizeof(fHeader), 1, file);
     fseek(file, fHandle->totalNumPages, SEEK_SET);
-    char * charArray =  calloc(PAGE_SIZE);
-    int i = 0;
+    char * charArray =  calloc(PAGE_SIZE, 1);
     int status = fwrite(charArray, 1, PAGE_SIZE, file);
     if(status != PAGE_SIZE){
         return RC_WRITE_FAILED;
