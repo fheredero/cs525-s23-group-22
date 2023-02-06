23b2c78911e801656f788de9170d5a62ec564065
Fernando Heredero López <fernandoheredero@thewiseseeker.com>
2023-02-06 10:43:36 -0600

    Update test_assign1_1.c
    Update test_assign1_1.c

--- assign1/test_assign1_1.c
+++ assign1/test_assign1_1.c
@@ -103,3 +103,69 @@ testSinglePageContent(void)
   
   TEST_DONE();
 }
+
+void 
+testSeveralPages(void) {
+
+  SM_FileHandle fh;
+  SM_PageHandle ph1, ph2;
+
+  testName = "test several page content";
+
+  // allocate memory for each page
+  ph1 = (SM_PageHandle) malloc(PAGE_SIZE);
+  ph2 = (SM_PageHandle) malloc(PAGE_SIZE);
+
+  // create a new pages file
+  TEST_CHECK(createPageFile (TESTPF));
+  TEST_CHECK(openPageFile (TESTPF, &fh));
+  printf("created and opened file\n");
+
+  // change ph1 to be a string and write that one to disk
+  for (i=0; i < PAGE_SIZE; i++)
+    ph1[i] = (i % 10) + '0';
+    ph[i] = (i % 60) + '0';
+
+  TEST_CHECK(writeBlock (1, &fh, ph1));
+  printf("writing first block\n");
+
+  // read back the page containing the string and check that it is correct
+  TEST_CHECK(readFirstBlock (&fh, ph1));
+  for (i=0; i < PAGE_SIZE; i++)
+    ASSERT_TRUE((ph1[i] == (i % 10) + '0'), "character in page read from disk is the one we expected.");
+  printf("reading first block\n");
+
+  // append a new block
+  TEST_CHECK(appendEmptyBlock(fh))
+  printf("second block is appended")
+  ASSERT_EQUALS_INT(fh.totalNumPages == 2)
+
+  // check that the second page is empty 
+  TEST_CHECK(readBlock(2, &fh, ph2));
+  for (i=0; i < PAGE_SIZE; i++)
+    ASSERT_TRUE((ph2[i] == 0), "expected zero byte in new page freshly initialized page for the appended block");
+  printf("second block was empty\n");
+
+  TEST_CHECK(readPreviousBlock(&fh, ph1));
+  printf("reading previous block\n");
+
+  TEST_CHECK(readCurrentBlock(&fh, ph1));
+  printf("reading current block\n");
+
+  TEST_CHECK(readNextBlock(&fh, ph1));
+  printf("reading next block\n");
+
+  // Check that the contents of ph1 are still correct
+  for ( int i = 0; i < PAGE_SIZE; i++){
+    ASSERT_TRUE(ph1[i] == (i % 10) + '0');
+  }
+
+  TEST_CHECK(getBlockPos(&fh) == fh.curPagePos);
+  TEST_CHECK(ensureCapacity(10,%fh))
+  ASSERT_EQUALS_INT(fh.totalNumPages == 10, "expected 10 pages in the file capacity");
+  printf("Capacity file has the correct number of pages")
+
+  // Destroy the new page file
+  TEST_CHECK(destroyPageFile (TESTPF));  
+
+}
